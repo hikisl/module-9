14eb5e82cb97c93296b15c8c91e69ed9a7cbd452
Name <slavaold1@yandex.ru>
2021-08-26 22:28:53 +0400

    dz final

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 
+
+
 namespace task1
 {
+    class MyException : Exception
+    {
+        public MyException(string message) : base(message) { }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var exception = new Exception[]
+            {
+                new IndexOutOfRangeException(),
+                 new InvalidOperationException(),
+                  new ArgumentOutOfRangeException(),
+                   new ArgumentNullException(),
+                    new MyException("Упс, что-то пошло не так!")
+
+            };
+
+            foreach (var item in exception)
+            {
+                try
+                {
+                    throw item;
+                }
+
+                catch (Exception ex) when (ex is IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Индекс находится за пределами границ массива или коллекции");
+                    Console.WriteLine(ex.Message);
+                }
+
+                catch(Exception ex) when (ex is InvalidOperationException)
+                {
+                    Console.WriteLine("Вызов метода недопустим в текущем состоянии объекта");
+                    Console.WriteLine(ex.Message);
+                }
+
+                catch (Exception ex)  when (ex is ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Аргумент находится за пределами диапазона допустимых значений.");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex) when (ex is ArgumentNullException)
+                {
+                    Console.WriteLine("	Аргумент, передаваемый в метод — null.");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex) when (ex is MyException)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
